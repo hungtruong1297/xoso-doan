b6081891c0cdcfe8ec153dce0547d29dbee6f79c
Hung Truong <hung.truong1297@gmail.com>
2022-01-13 15:32:56 +0700

    Add check duplicated user

--- src/main/java/com/hungfunix/xosodemo/controller/AdminController.java
+++ src/main/java/com/hungfunix/xosodemo/controller/AdminController.java
@@ -4,9 +4,12 @@ import com.hungfunix.xosodemo.model.User;
 import com.hungfunix.xosodemo.repository.RoleRepository;
 import com.hungfunix.xosodemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -22,14 +25,22 @@ public class AdminController {
     private BCryptPasswordEncoder passwordEncoder;
 
     @PostMapping("/add")
-    public void addUserByAdmin(@RequestBody User user) {
+    public ResponseEntity<String> addUserByAdmin(@RequestBody User user) {
 
-        String pwd = user.getPassword();
-        String encryptPwd = passwordEncoder.encode(pwd);
-        user.setPassword(encryptPwd);
+        if (this.userRepository.findUserByMail(user.getMail()) == null) {
 
-        userRepository.save(user);
-        System.out.println("User added.");
+            String pwd = user.getPassword();
+            String encryptPwd = passwordEncoder.encode(pwd);
+            user.setPassword(encryptPwd);
+
+            userRepository.save(user);
+            System.out.println("User added.");
+            return new ResponseEntity<>("User added.", HttpStatus.OK);
+        }
+
+        else {
+            return new ResponseEntity<>("User not added. Duplicated user.", HttpStatus.CONFLICT);
+        }
     }
 
     @GetMapping("")
