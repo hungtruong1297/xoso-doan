d87ff4adc0ee731d1f3c3c6c19ceb93476308443
hung.truongngoc <hung.truongngoc@ncc.asia>
2022-02-23 09:29:43 +0700

    Add file CorsConfig

--- src/main/java/com/hungfunix/xosodemo/XosoDemoApplication.java
+++ src/main/java/com/hungfunix/xosodemo/XosoDemoApplication.java
@@ -3,12 +3,14 @@ package com.hungfunix.xosodemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableWebSecurity
-@CrossOrigin(origins="*")
 public class XosoDemoApplication {
 
     public static void main(String[] args) {
